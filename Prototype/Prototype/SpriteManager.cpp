bbd742764901eebe728bff7e90d2742bf8af6e7b
a.t.c_92@hotmail.com <a.t.c_92@hotmail.com>
2015-01-21 13:14:24 +0100

    Create Sprite

--- Prototype/Prototype/SpriteManager.cpp
+++ Prototype/Prototype/SpriteManager.cpp
@@ -15,7 +15,23 @@ SpriteManager::~SpriteManager()
 
 sf::Sprite* SpriteManager::CreateSprite(const std::string& filename, int x, int y, int w, int h)
 {
+	auto it = m_textures.find(filename);
+	if (it == m_textures.end())
+	{
+		sf::Texture* texture = new sf::Texture();
+		texture->loadFromFile(filename);
+
+		m_textures.insert(std::pair<std::string, sf::Texture*>(filename, texture));
+		it = m_textures.find(filename);
+	}
+
+	sf::Sprite* sprite = new sf::Sprite();
+	sprite->setTexture(*it->second);
 	
+	sf::IntRect rect = sf::IntRect(x, y, w, h);
+	sprite->setTextureRect(rect);
+	
+	return sprite;
 }
 
 void SpriteManager::DestrySprite()
